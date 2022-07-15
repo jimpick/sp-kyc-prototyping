b314ab8dd12b231e8406570e8fc39225e29e068d
Jim Pick <jim@jimpick.com>
2022-07-14 22:28:01 -0700

    Fix script

--- manual-run.sh
+++ manual-run.sh
@@ -6,7 +6,7 @@
 
 curl \
   -X POST \
-  -H "Accept: application/vnd.github+json" \ 
+  -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $GITHUB_PAT" \
   https://api.github.com/repos/jimpick/sp-kyc-prototyping/dispatches \
   -d '{"event_type":"manual-run"}'
