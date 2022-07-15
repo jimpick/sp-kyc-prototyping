d58df8c7004e92d625feca07bfe75cd01200476c
Jim Pick <jim@jimpick.com>
2022-07-14 22:25:26 -0700

    Add manual run script

---
+++ manual-run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# https://docs.github.com/en/rest/repos/repos#create-a-repository-dispatch-event
+
+. .env
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \ 
+  -H "Authorization: token $GITHUB_PAT" \
+  https://api.github.com/repos/jimpick/sp-kyc-prototyping/dispatches \
+  -d '{"event_type":"manual-run"}'
